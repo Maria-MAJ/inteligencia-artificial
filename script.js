60e58f1be7eee87431f0806fd91a244a10f50abe
Maria-MAJ <129186758+Maria-MAJ@users.noreply.github.com>
2024-06-13 19:54:05 -0300

    ok

--- script.js
+++ script.js
@@ -4,20 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia"
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
-            }
-        ]
-},
 
 
 const perguntas = [
