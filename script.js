9985c915f0a985fc5bacd557e5fb24d1cfff5fee
Maria-MAJ <129186758+Maria-MAJ@users.noreply.github.com>
2024-06-13 19:33:44 -0300

    ok

--- script.js
+++ script.js
@@ -79,13 +79,9 @@ function mostraAlternativas(){
     }
 
 
-function mostraAlternativas() {
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
+
     }
-}
+
 
 mostraPergunta();
 
