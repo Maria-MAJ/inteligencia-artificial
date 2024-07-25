2e5f178be5f2d90e128859436394eab0c3e04b6d
Maria-MAJ <129186758+Maria-MAJ@users.noreply.github.com>
2024-07-25 19:38:53 -0300

    ok

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ const perguntas = [
       alternativas: [
         {
           texto: "Isso é assustador!",
-          afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+            afirmacao: [ "No início ficou com medo do que essa tecnologia pode fazer.",
+            "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+            ],
         },
         {
           texto: "Isso é maravilhoso!",
-          afirmacao: "Quis saber como usar IA no seu dia a dia."
+          afirmacao: 
+          "Quis saber como usar IA no seu dia a dia.",
+          "Pensou que IA pode ajudar em tarefas da sua vida."
         },
       ],
     },
