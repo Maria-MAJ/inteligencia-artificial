17156daa76d1e2550f121db164f566e810bb30d4
Maria-MAJ <129186758+Maria-MAJ@users.noreply.github.com>
2024-08-22 19:37:15 -0300

    ok

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,20 +1,20 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu do seu País, você se depara com uma ponte de Madeira, e uma estrada, qual caminho você escolhe:
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "A ponte!",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                    "No início ficou com medo onde esssa ponte poderia levar.",
+                    "Achou assustador pensar na velocidade na qual estava a andar sobre ela."
                 ],
                 proxima: 1,
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "A estrada!",
                 afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                    "Quis saber onde a estrada o levaria .",
+                    "Pensou que a estrada  era mais um lugar onde não deveria ir , pois parecia tenebrosa."
                 ],
                 proxima: 2,
             },
